9c4ab1980d658de59a3903dd96638688b69f63ee
Yanita Kyuchukova <111355885+2201280307070703@users.noreply.github.com>
2024-03-26 14:34:49 +0200

    add toastr for notification messages

---
+++ CarRentingSystem.Common/NotificationMessagesConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace CarRentingSystem.Common
+{
+    public static class NotificationMessagesConstants
+    {
+        public const string SuccessMessage = "SuccessMessage";
+        public const string WarningMessage = "WarningMessage";
+        public const string InformationMessage = "InformationMessage";
+        public const string ErrorMessage = "ErrorMessage";
+    }
+}
