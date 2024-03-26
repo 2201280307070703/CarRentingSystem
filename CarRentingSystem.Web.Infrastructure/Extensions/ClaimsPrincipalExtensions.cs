b3893ed549825cabaa9213e9222620de5b14633a
Yanita Kyuchukova <111355885+2201280307070703@users.noreply.github.com>
2024-03-26 13:57:02 +0200

    add claims principal extensions

---
+++ CarRentingSystem.Web.Infrastructure/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace CarRentingSystem.Web.Infrastructure.Extensions
+{
+    using System.Security.Claims;
+
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string GetUserId(this ClaimsPrincipal user)
+        {
+            return user.FindFirst(ClaimTypes.NameIdentifier)!.ToString();
+        }
+    }
+}
